a25925aa676ce91551481ac42630310fc8700703
Junho Lee <ghzm888@gmail.com>
2023-07-28 21:41:14 +0900

    add netlify.toml

--- fine-tuning-chatbot/next.config.js
+++ fine-tuning-chatbot/next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {}
+const nextConfig = {}
 
-// module.exports = nextConfig
-module.exports = {
-    future: {
-        webpack5: true, // 빌드 캐시를 활성화하려면 webpack5를 true로 설정합니다.
-    },
-};
+module.exports = nextConfig
