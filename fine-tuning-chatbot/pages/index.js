2b3cef5126869312e816df043d7d2b879a3123ca
Junho Lee <ghzm888@gmail.com>
2023-08-02 00:11:29 +0900

    add netlify.toml

--- fine-tuning-chatbot/pages/index.js
+++ fine-tuning-chatbot/pages/index.js
@@ -8,11 +8,6 @@ const Page1 = () => {
             <Link href="/index2">
                 <a>Go to Page 2</a>
             </Link>
-            <div>
-                {/* 이곳에 Page 1에서 필요한 추가적인 HTML 코드를 작성하세요 */}
-                <p>Welcome to Page 1</p>
-                <button onClick={() => alert('Button Clicked!')}>Click Me</button>
-            </div>
         </div>
     );
 };
