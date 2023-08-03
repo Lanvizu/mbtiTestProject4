cd670b923ca4c092c712c543d6923c1840d2711d
Junho Lee <ghzm888@gmail.com>
2023-08-03 15:45:28 +0900

    add netlify.toml

--- fine-tuning-chatbot/pages/tset2.js
+++ fine-tuning-chatbot/pages/tset2.js
@@ -5,23 +5,22 @@ import ChatApp from "@/pages/index2";
 // kakao 기능 동작을 위해 넣어준다.
 
 
+// export default () =>{
+//     // 배포한 자신의 사이트
+//     const realUrl = "https://master--mbtitestchat.netlify.app/"
+//     // 로컬 주소 (localhost 3000 같은거)
+//     const resultUrl = window.location.href;
+//
+//     // 재랜더링시에 실행되게 해준다.
+//     useEffect(()=>{
+//         // init 해주기 전에 clean up 을 해준다.
+//         Kakao.cleanup();
+//         // 자신의 js 키를 넣어준다.
+//         Kakao.init('0b8734b12b69c143778a10e9e76f4abd');
+//         // 잘 적용되면 true 를 뱉는다.
+//         console.log(Kakao.isInitialized());
+//     },[]);
 function kakaoShare(){
-    // export default () =>{
-    //     // 배포한 자신의 사이트
-    //     const realUrl = "https://master--mbtitestchat.netlify.app/"
-    //     // 로컬 주소 (localhost 3000 같은거)
-    //     const resultUrl = window.location.href;
-    //
-    //     // 재랜더링시에 실행되게 해준다.
-    //     useEffect(()=>{
-    //         // init 해주기 전에 clean up 을 해준다.
-    //         Kakao.cleanup();
-    //         // 자신의 js 키를 넣어준다.
-    //         Kakao.init('0b8734b12b69c143778a10e9e76f4abd');
-    //         // 잘 적용되면 true 를 뱉는다.
-    //         console.log(Kakao.isInitialized());
-    //     },[]);
-
 
     const { Kakao } = window;
     const shareKakao = () =>{
@@ -59,4 +58,4 @@ function kakaoShare(){
         </>
     )
 }
-export default kakaoShare();
+export default kakaoShare;
